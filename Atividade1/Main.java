3fa674e77daaf3b4ad4f5d826ec1f7262e9703bb
Hiagolaurindo <lucasgeral545@gmail.com>
2025-09-10 22:20:29 -0300

    add main.java

--- Atividade1/Main.java
+++ Atividade1/Main.java
@@ -5,13 +5,13 @@ public class Main {
         Computador pc = new Computador(16, 512, 4, 2.5f);
         SistemaOperacional so = new SistemaOperacional(pc);
 
-        Programa p1 = new Programa(8, 100, 2, 1000);
+        Programa p1 = new Programa(8, 100, 1000);
         so.executarPrograma(p1);
 
-        Programa p2 = new Programa(8, 600, 2, 2000);
+        Programa p2 = new Programa(8, 600, 2000);
         so.executarPrograma(p2);
 
-        Programa p3 = new Programa(32, 200, 4, 3000);
+        Programa p3 = new Programa(32, 200, 3000);
         so.executarPrograma(p3);
     }
 }
